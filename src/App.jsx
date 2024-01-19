7508525c694d6a73374669cb37a0b8c692df2cf9
callummiles <cwmiles18@gmail.com>
2024-01-18 21:55:19 -0600

    data populating in front end web app, need to add more stats now and update formatting

--- src/App.jsx
+++ src/App.jsx
@@ -30,19 +30,30 @@ function App() {
   //   </>
   // );
 
+  // const [data, setData] = useState({
+  //   squadData: {},
+  //   playerData: {},
+  // });
+  const [dataC, setDataC] = useState([]);
+  const [dataP, setDataP] = useState([]);
+
   return (
     <div id="app">
       <h1>EPL PLAYER SEARCH</h1>
       <p>For each player search, please enter club and player name...</p>
-      <SearchBoard />
+      <SearchBoard
+        dataC={dataC}
+        setDataC={setDataC}
+        dataP={dataP}
+        setDataP={setDataP}
+      />
     </div>
   );
 }
 
-const SearchBoard = () => {
+const SearchBoard = ({ dataC, setDataC, dataP, setDataP }) => {
   const [searchTermC, setSearchTermC] = useState('');
   const [searchTermP, setSearchTermP] = useState('');
-  const [personData, setPersonData] = useState(null);
 
   const teamIDs = {
     'Brighton & Hove Albion': 78,
@@ -67,20 +78,20 @@ const SearchBoard = () => {
     Burnley: 27,
   };
 
-  const data = {
-    squadData: {},
-    playerData: {},
-  };
-
   const handleSearchClubs = async () => {
     try {
-      console.log('Attempting to find data: ', { searchTermC });
+      console.log('Attempting to find squad data: ', { searchTermC });
       const searchID = teamIDs[searchTermC];
       const response = await fetch(
         `http://localhost:8000/squaddata/${searchID}`
       );
-      data.squadData = await response.json();
-      console.log(data.squadData);
+      const squadData = await response.json();
+      console.log(`${searchTermC} squad data: `, squadData);
+
+      setDataC((prevData) => ({
+        ...prevData,
+        squadData: squadData || [],
+      }));
     } catch (error) {
       console.error('Error fetching squad data: ', error);
     }
@@ -88,34 +99,44 @@ const SearchBoard = () => {
 
   const handleSearchPlayers = async () => {
     try {
-      console.log('Attempting to find data: ', { searchTermP });
+      console.log('Attempting to find player data: ', { searchTermP });
       await handleSearchClubs();
-      // console.log(
-      //   'Attempt to find squad inside player search:',
-      //   data.squadData
-      // );
-      //console.log(data.squadData.squadData.data);
+      //console.log('Attempt to find squad inside player search:', dataC);
+      //console.log(dataC.squadData.squadData.data);
       let searchID = '';
-      const playerSearchData = data.squadData.squadData.data;
+      console.log(dataC);
+      const playerSearchData = dataC.squadData.squadData.data || [];
       for (let i = 0; i < playerSearchData.length; i++) {
         //console.log(data.squadData.squadData.data[i].player.name);
         //console.log(searchTermP);
-        if (searchTermP === playerSearchData[i].player.name) {
-          console.log(playerSearchData[i].player);
+        if (
+          searchTermP === playerSearchData[i].player.display_name ||
+          searchTermP === playerSearchData[i].player.name
+        ) {
+          console.log(
+            `${searchTermP} player info: `,
+            playerSearchData[i].player
+          );
           searchID = playerSearchData[i].player.id;
         }
       }
-      //const searchID = '1494245';
+      console.log(searchID);
       const response = await fetch(
         `http://localhost:8000/playerdata/${searchID}`
       );
-      data.playerData = await response.json();
-      console.log(data.playerData);
+      const playerData = await response.json();
+      console.log(`${searchTermP} player data: `, playerData);
+
+      setDataP((prevData) =>
+        // ...prevData,
+        // playerData: playerData || [],
+        [...prevData, playerData || []]
+      );
     } catch (error) {
       console.error('Error fetching player data: ', error);
     }
   };
-
+  //console.log(dataP);
   return (
     <div>
       <SearchClub
@@ -131,7 +152,10 @@ const SearchBoard = () => {
         onSearchChangeP={setSearchTermP}
         onSearchClickP={handleSearchPlayers}
       />
-      <SearchResult />
+      <p></p>
+      <p>---</p>
+      <p></p>
+      <SearchResult dataP={dataP} />
     </div>
   );
 };
@@ -168,11 +192,70 @@ const SearchPlayer = ({ searchTermP, onSearchChangeP, onSearchClickP }) => {
   );
 };
 
-const SearchResult = () => {
+const SearchResult = ({ dataP }) => {
+  //console.log(dataP);
   return (
-    <div>
-      <h2>Player Details</h2>
-      <p>Name:</p>
+    //console.log()
+    <div className="SearchResult">
+      <p></p>
+      <h2>Player Statistics</h2>
+      <p></p>
+
+      {dataP.map((player, index) => {
+        // for (let key in player.playerData.data.statistics[0].details) {
+        //   console.log(
+        //     `${key}: ${JSON.stringify(
+        //       player.playerData.data.statistics[0].details[key]
+        //     )}`
+        //   );
+
+        // }
+        let goalStat = 0;
+        let appStat = 0;
+        let startStat = 0;
+        let minsStat = 0;
+
+        const playerStats = player.playerData.data.statistics[0].details;
+        for (let i = 0; i < playerStats.length; i++) {
+          console.log(playerStats[i].type);
+          if ('Goals' === playerStats[i].type.name) {
+            //console.log(playerStats[i].value.total);
+            goalStat = playerStats[i].value.total;
+          }
+          if ('Appearances' === playerStats[i].type.name) {
+            appStat = playerStats[i].value.total;
+          }
+          if ('Lineups' === playerStats[i].type.name) {
+            startStat = playerStats[i].value.total;
+          }
+          if ('Minutes Played' === playerStats[i].type.name) {
+            minsStat = playerStats[i].value.total;
+          }
+        }
+
+        let mpgStat = Math.round(minsStat / appStat);
+        let gpmStat = Math.round(minsStat / goalStat);
+
+        return (
+          <div key={index}>
+            <p>Name: {player.playerData.data.name}</p>
+            <p>Position: {player.playerData.data.position.name}</p>
+            <p>-</p>
+            <p>
+              Appearances: {appStat} ({startStat} starts)
+            </p>
+            <p>
+              Minutes Played: {minsStat} ({mpgStat} per game)
+            </p>
+            <p>-</p>
+            <p>
+              Goals: {goalStat} ({gpmStat} mins per goal)
+            </p>
+            <p>Passes: {player.passes}</p>
+            <p>Tackles: {player.tackles}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
